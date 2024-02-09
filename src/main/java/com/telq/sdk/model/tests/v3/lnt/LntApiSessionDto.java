da46c26b49bdae4d24605cdcece0cf33e83a0ec0
mann <4lexander.mann@gmail.com>
2024-02-09 20:22:37 +0700

    TP-7732 add session dto

---
+++ src/main/java/com/telq/sdk/model/tests/v3/lnt/LntApiSessionDto.java
@@ -0,0 +1,26 @@
+package com.telq.sdk.model.tests.v3.lnt;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class LntApiSessionDto {
+
+    private Long smppSessionId;
+    private String hostIp;
+    private Integer hostPort;
+    private String systemId;
+    private String systemType;
+    private Integer throughput;
+    private Byte destinationTon;
+    private Byte destinationNpi;
+    private Boolean enabled;
+    private Long userId;
+    private Boolean online;
+    private String lastError;
+    private Integer windowSize;
+    private Boolean useSSL;
+    private Long windowWaitTimeout;
+    private Integer supplierCount;
+}
