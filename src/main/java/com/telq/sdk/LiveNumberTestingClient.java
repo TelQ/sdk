2843f1e52160bd606c3aad17021bc431700636aa
mann <4lexander.mann@gmail.com>
2024-02-12 20:54:10 +0700

    TP-7732 add LiveNumberTestingClient interface

---
+++ src/main/java/com/telq/sdk/LiveNumberTestingClient.java
@@ -0,0 +1,30 @@
+package com.telq.sdk;
+
+import com.telq.sdk.model.tests.v3.lnt.*;
+
+import java.time.Instant;
+import java.util.List;
+
+public interface LiveNumberTestingClient {
+    List<LntApiCreateTestResponseDto> createTests(LntApiTestRequestDto testRequestDto);
+    Page<LntApiTestResultDto> getTestResults(PageConf pageConf, Instant from, Instant to);
+    LntApiTestResultDto getTestResultById(String testId);
+
+    LntApiSessionDto createSession(LntApiCreateSessionDto sessionDto);
+    LntApiSessionDto updateSession(LntApiCreateSessionDto sessionDto);
+    Page<LntApiSessionDto> getSessions(PageConf pageConf);
+    LntApiSessionDto getSessionById(String sessionId);
+    void deleteSessionById(String sessionId);
+
+    LntApiSupplierDto createSupplier(LntApiCreateSupplierDto supplierDto);
+    LntApiSupplierDto updateSupplier(LntApiCreateSupplierDto supplierDto);
+    Page<LntApiSupplierDto> getSuppliers(PageConf pageConf);
+    LntApiSupplierDto getSupplierById(Long supplierId);
+    List<LntApiSupplierDto> getSuppliersBySessionId(Long sessionId);
+    void deleteSupplierById(Long supplierId);
+
+    void assignSuppliersToSession(LntApiAssignSuppliersDto assignSuppliersDto);
+
+    Page<LntApiSessionSupplierDto> getSessionSuppliers(PageConf pageConf);
+
+}
